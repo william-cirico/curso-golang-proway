3c83b30fa54dda8f880dbda801d3139a4b6217d6
william <william.cirico@proway.com.br>
2022-12-01 17:49:58 -0300

    adicionando exemplo de funções variádicas

--- Aula-01/funcoes/main.go
+++ Aula-01/funcoes/main.go
@@ -10,11 +10,18 @@ func trocarOrdem(x, y int) (int, int) {
 	return y, x
 }
 
+// Função variádica
+func mostraNomes(nomes ...string) {
+	for _, nome := range nomes {
+		fmt.Println(nome)
+	}
+}
+
 func main() {
 	// Defer adia a chamada da função
 	defer fmt.Println("Sou executado depois :d")
 
 	fmt.Println(somar(10, 2))
 	fmt.Println(trocarOrdem(10, 2))
-
+	mostraNomes("João", "Maria")
 }
